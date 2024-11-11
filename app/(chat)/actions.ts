88cf33a575de028ce3abde2c95ad6d30d17fd2d0
ashura <cotopessergo@gmail.com>
2024-11-11 19:13:35 +0300

    add time sleep

--- app/(chat)/actions.ts
+++ app/(chat)/actions.ts
@@ -5,19 +5,28 @@ import { cookies } from 'next/headers';
 
 import { customModel } from '@/ai';
 
+// Функция задержки, принимает время в миллисекундах
+function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Сохранение ID модели в cookies
 export async function saveModelId(model: string) {
   const cookieStore = await cookies();
   cookieStore.set('model-id', model);
 }
 
+// Генерация заголовка на основе первого сообщения пользователя с задержкой перед запросом к API
 export async function generateTitleFromUserMessage({
-  message,
-}: {
+                                                     message,
+                                                   }: {
   message: CoreUserMessage;
 }) {
+  await sleep(500);
+
   const { text: title } = await generateText({
     model: customModel('gpt-3.5-turbo'),
-    system: `\n
+    system: `
     - you will generate a short title based on the first message a user begins a conversation with
     - ensure it is not more than 80 characters long
     - the title should be a summary of the user's message
